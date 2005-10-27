4dd9e6a60913df0ce6bbf435039cd54040738b48
david <>
2005-10-27 21:18:17 +0000

    * 0.conf: 	- Update version * src/sakura.c: 	- Fixed font increase/decrease code. Added destroy and del_tab 	  functions. Load font at init time.

--- src/sakura.c
+++ src/sakura.c
@@ -11,46 +11,73 @@ static struct {
 	gint size;
 } sakura;
 
+/* Callbacks */
+static void sakura_increase_font (GtkWidget *, void *);
+static void sakura_decrease_font (GtkWidget *, void *);
+static void sakura_child_exited (GtkWidget *, void *);
+
+/* Functions */	
+static void sakura_init();
+static void sakura_destroy();
+static void sakura_add_tab();
+static void sakura_del_tab();
+static void sakura_set_font();
+
+
 static void sakura_increase_font (GtkWidget *widget, void *data)
 {
-	int page=gtk_notebook_get_current_page(GTK_NOTEBOOK(sakura.notebook));
-	gchar *fontstring;
-	
 	sakura.size += 1;
-
-	SAY("Ctrl +");
-	fontstring=g_strdup_printf("%s %d", sakura.font, sakura.size);
-	vte_terminal_set_font_from_string(gtk_notebook_get_nth_page(sakura.notebook, page), fontstring);
-	g_free(fontstring);
+	sakura_set_font();
 }
 
 static void sakura_decrease_font (GtkWidget *widget, void *data)
 {
-	int page=gtk_notebook_get_current_page(GTK_NOTEBOOK(sakura.notebook));
-	gchar *fontstring;
-	
 	sakura.size -= 1;
-	
-	SAY("Ctrl -");
-	fontstring=g_strdup_printf("%s %d", sakura.font, sakura.size);
-	vte_terminal_set_font_from_string(gtk_notebook_get_nth_page(sakura.notebook, page), fontstring);
-	g_free(fontstring);
+	sakura_set_font();
 }
 
 static void sakura_child_exited (GtkWidget *widget, void *data)
 {
 	SAY("Child exited.Who knows what child...");
+
+    if ( gtk_notebook_get_n_pages(GTK_NOTEBOOK(sakura.notebook)) == 1) {
+		/* Last terminal was closes so...*/
+		sakura_destroy();
+	} else {
+		sakura_del_tab();
+	}	
 }
 
 static void sakura_init()
 {
 	sakura.main_window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	sakura.notebook=gtk_notebook_new();
-	sakura.font=g_strdup("BitStream Vera Sans");
-	sakura.size=12;
+	sakura.font=g_strdup("Bitstream Vera Sans Mono");
+	sakura.size=13;
 
 	gtk_container_add(GTK_CONTAINER(sakura.main_window), sakura.notebook);
 }
+
+static void sakura_destroy()
+{
+	/* TODO: ¿Destroy terminal? */
+	gtk_widget_destroy(sakura.main_window);
+	g_free(sakura.font);
+
+	gtk_main_quit();
+}
+
+static void sakura_set_font()
+{
+	int page=gtk_notebook_get_current_page(GTK_NOTEBOOK(sakura.notebook));
+	gchar *fontstring;
+	
+	fontstring=g_strdup_printf("%s %d", sakura.font, sakura.size);
+	vte_terminal_set_font_from_string(VTE_TERMINAL(gtk_notebook_get_nth_page(GTK_NOTEBOOK(sakura.notebook),
+			   						  page)), fontstring);
+	g_free(fontstring);
+}
+
 
 static void sakura_add_tab()
 {
@@ -73,6 +100,12 @@ static void sakura_add_tab()
 	g_signal_connect(G_OBJECT(terminal), "child-exited", G_CALLBACK(sakura_child_exited), NULL);
 }
 
+static void sakura_del_tab()
+{
+	gtk_notebook_remove_page(GTK_NOTEBOOK(sakura.notebook),
+							 gtk_notebook_get_current_page(GTK_NOTEBOOK(sakura.notebook)));
+}
+
 int main(int argc, char **argv)
 {
 	gtk_init(&argc, &argv);
@@ -82,6 +115,7 @@ int main(int argc, char **argv)
 
 	/* Add first tab */
 	sakura_add_tab();
+	sakura_set_font();
 
 	gtk_widget_show_all(sakura.main_window);
 	gtk_main();
