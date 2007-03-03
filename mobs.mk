fdaa82fb201aaf9d92ea95d5620dc8f781763652
david <>
2007-03-03 16:56:52 +0000

    * 0, mobs.h, mobs.mk: 	- Update mobs to the latest version

--- mobs.mk
+++ mobs.mk
@@ -1,26 +1,24 @@
-#   $Revision: 60 $
+#   $Revision: 67 $
 #   This file contains helpers for building a makefile.
 #
 #   Copyright (C) 2005,2006 Rau'l Nu'n~ez de Arenas Coronado
 #   Report bugs to DervishD <bugs@dervishd.net>
 #
-#       This program is free software; you can redistribute it and/or
-#        modify it under the terms of the GNU General Public License
-#               as published by the Free Software Foundation;
-#                     either version 2 of the License,
-#                  or (at your option) any later version.
+#        This program is free software; you can redistribute it and/or
+#         modify it under the terms of the GNU General Public License
+#           version 2 as published by the Free Software Foundation.
 #
-#      This program is distributed in the hope that it will be useful,
-#        but WITHOUT ANY WARRANTY; without even the implied warranty
-#          of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#           See the GNU General Public License for more details.
+#       This program is distributed in the hope that it will be useful,
+#         but WITHOUT ANY WARRANTY; without even the implied warranty
+#           of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#            See the GNU General Public License for more details.
 #
-#     You should have received a copy of the GNU General Public License
-#            ('GPL') along with this program; if not, write to:
+#      You should have received a copy of the GNU General Public License
+#             ('GPL') along with this program; if not, write to:
 #
-#                      Free Software Foundation, Inc.
-#                        59 Temple Place, Suite 330
-#                        Boston, MA 02111-1307  USA
+#                       Free Software Foundation, Inc.
+#                         59 Temple Place, Suite 330
+#                         Boston, MA 02111-1307  USA
 ################
 .PHONY:
 .DELETE_ON_ERROR:
