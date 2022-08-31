ab609f13dd8bce310daff91763c20c66f23e02a7
David Gómez <dabisu@gmail.com>
2022-08-31 20:08:34 +0200

    Put back the command line title option
    Update translations

--- src/sakura.c
+++ src/sakura.c
@@ -285,6 +285,7 @@ static struct {
 	GKeyFile *cfg;
 	char *configfile;
 	char *icon;
+	char *main_title;		/* Main window static title from user input */
 	gchar *tab_default_title;
 	gint add_tab_accelerator;
 	gint del_tab_accelerator;
@@ -500,6 +501,7 @@ static void     sakura_show_scrollbar (void);
 static const char *option_font;
 static const char *option_workdir;
 static const char *option_execute;
+static const char *option_title;
 static gchar **option_xterm_args;
 static gboolean option_xterm_execute=FALSE;
 static gboolean option_version=FALSE;
@@ -516,6 +518,7 @@ static gint option_colorset;
 
 static GOptionEntry entries[] = {
 	{ "version", 'v', 0, G_OPTION_ARG_NONE, &option_version, N_("Print version number"), NULL },
+	{ "title", 't', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_STRING, &option_title, N_("Set window title"), NULL},
 	{ "font", 'f', 0, G_OPTION_ARG_STRING, &option_font, N_("Select initial terminal font"), NULL },
 	{ "ntabs", 'n', 0, G_OPTION_ARG_INT, &option_ntabs, N_("Select initial number of tabs"), NULL },
 	{ "working-directory", 'd', 0, G_OPTION_ARG_STRING, &option_workdir, N_("Set working directory"), NULL },
@@ -526,7 +529,7 @@ static GOptionEntry entries[] = {
 	{ "icon", 'i', 0, G_OPTION_ARG_STRING, &option_icon, N_("Set window icon"), NULL },
 	{ "columns", 'c', 0, G_OPTION_ARG_INT, &option_columns, N_("Set columns number"), NULL },
 	{ "rows", 'r', 0, G_OPTION_ARG_INT, &option_rows, N_("Set rows number"), NULL },
-	{ "hold", 'h', 0, G_OPTION_ARG_NONE, &option_hold, N_("Hold window after execute command"), NULL },
+	{ "hold", 'h', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &option_hold, N_("Hold window after execute command"), NULL },
 	{ "maximize", 'm', 0, G_OPTION_ARG_NONE, &option_maximize, N_("Maximize window"), NULL },
 	{ "fullscreen", 's', 0, G_OPTION_ARG_NONE, &option_fullscreen, N_("Fullscreen mode"), NULL },
 	{ "config-file", 0, 0, G_OPTION_ARG_FILENAME, &option_config_file, N_("Use alternate configuration file"), NULL },
@@ -804,9 +807,13 @@ sakura_switch_page_cb (GtkWidget *widget, GtkWidget *widget_page, guint page_num
 
 	sk_tab = sakura_get_sktab(sakura, page_num);
 
-	/* Update the window title when a new tab is selected, but don't override the default */
-	if (!sakura.tab_default_title)
-		gtk_window_set_title(GTK_WINDOW(sakura.main_window), gtk_label_get_text(GTK_LABEL(sk_tab->label)));
+	/* Update the window title when a new tab is selected, but don't when an user title has been set */
+	//if (!sakura.tab_default_title && !sakura.main_title)
+	if (!sakura.main_title) {
+		if (g_strcmp0(gtk_label_get_text(GTK_LABEL(sk_tab->label)),"")!=0) {
+			gtk_window_set_title(GTK_WINDOW(sakura.main_window), gtk_label_get_text(GTK_LABEL(sk_tab->label)));
+		}
+	}
 
 }
 
@@ -1082,7 +1089,7 @@ sakura_title_changed_cb (GtkWidget *widget, void *data)
 	/* User set values overrides any other one */
 	if (!sk_tab->label_set_byuser) {
 		sakura_set_tab_label_text(tabtitle, modified_page);
-		gtk_window_set_title(GTK_WINDOW(sakura.main_window), tabtitle);
+		if (!sakura.main_title) gtk_window_set_title(GTK_WINDOW(sakura.main_window), tabtitle);
 	}
 
 }
@@ -1222,7 +1229,8 @@ sakura_set_name_dialog_cb (GtkWidget *widget, void *data)
 	if (response == GTK_RESPONSE_ACCEPT) {
 		sakura_set_tab_label_text(gtk_entry_get_text(GTK_ENTRY(entry)), page);
 		gtk_window_set_title(GTK_WINDOW(sakura.main_window), gtk_entry_get_text(GTK_ENTRY(entry)));
-		sk_tab->label_set_byuser=true;
+		sk_tab->label_set_byuser=true; 
+		sakura.main_title=NULL; /* Ignore the user-set window title if the user names the tab */
 	}
 
 	gtk_widget_destroy(input_dialog);
@@ -2040,6 +2048,7 @@ sakura_init()
 	}
 	sakura.cursor_type = g_key_file_get_integer(sakura.cfg, cfg_group, "cursor_type", NULL);
 
+	/* Only in config file */
 	if (!g_key_file_has_key(sakura.cfg, cfg_group, "word_chars", NULL)) {
 		sakura_set_config_string("word_chars", DEFAULT_WORD_CHARS);
 	}
@@ -2058,6 +2067,7 @@ sakura_init()
 	}
 	sakura.palette = palettes[sakura.palette_idx];
 
+	/* Keybindings are only in the config file */
 	if (!g_key_file_has_key(sakura.cfg, cfg_group, "add_tab_accelerator", NULL)) {
 		sakura_set_config_integer("add_tab_accelerator", DEFAULT_ADD_TAB_ACCELERATOR);
 	}
@@ -2188,7 +2198,7 @@ sakura_init()
 	}
 	sakura.menu_button = g_key_file_get_integer(sakura.cfg, cfg_group, "menu_button", NULL);
 
-	/* NULL if not found. Don't add a new one */
+	/* NULL if not found. Don't add a new one */ /* Only in config file */
 	sakura.tab_default_title = g_key_file_get_string(sakura.cfg, cfg_group, "tab_default_title", NULL);
 
 	sakura.dont_save = g_key_file_get_boolean(sakura.cfg, cfg_group, "dont_save", NULL);
@@ -2258,14 +2268,6 @@ sakura_init()
 	}
 	sakura.argv[2]=NULL;
 
-	if (option_columns) {
-		sakura.columns = option_columns;
-	}
-
-	if (option_rows) {
-		sakura.rows = option_rows;
-	}
-
 	/* Add datadir path to icon name and set icon */
 	gchar *icon_path; gerror=NULL;
 	if (option_icon) {
@@ -2276,6 +2278,22 @@ sakura_init()
 	gtk_window_set_icon_from_file(GTK_WINDOW(sakura.main_window), icon_path, &gerror);
 	g_free(icon_path); icon_path=NULL;
 	if (gerror) g_error_free(gerror);
+
+	/* More options */
+	if (option_title) {
+		sakura.main_title = g_strdup_printf("%s", option_title);
+		gtk_window_set_title(GTK_WINDOW(sakura.main_window), sakura.main_title);
+	} else {
+		sakura.main_title = NULL;
+	}
+
+	if (option_columns) {
+		sakura.columns = option_columns;
+	}
+
+	if (option_rows) {
+		sakura.rows = option_rows;
+	}
 
 	if (option_font) {
 		sakura.font=pango_font_description_from_string(option_font);
